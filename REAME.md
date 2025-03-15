88452f20045bb628c820431994e232365db4e8b5
VMjavier <110652153+VMjavier@users.noreply.github.com>
2025-03-14 22:54:43 -0500

    Reto terminado y creación del README.md

---
+++ REAME.md
@@ -0,0 +1,8 @@
+<h1>Reto amigo secreto</h1>
+
+### Instrucciones
+
+* Ingresar el nombre de la persona a registrar.
+* Con el boton anadir los vas guardando en la lista.
+* Cuando termines de registrar todos los nombres presionamos el boton Sorteo de Amigo.
+* En pantalla se mostrará el nombre del amigo secreto.
--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -4,6 +4,7 @@ let amigos = [];
 const inputNombreAmigo = document.getElementById('amigo');
 const botonAgregarAmigo = document.getElementById('agregarAmigo');
 const listaAmigos = document.getElementById('listaAmigos');
+const resultado = document.getElementById('resultado');
 
 // Función para agregar un amigo
 function agregarAmigo() {
@@ -30,6 +31,22 @@ function mostrarAmigos() {
         elemento.textContent = amigo;
         listaAmigos.appendChild(elemento);
     });
+}
+
+function sortearAmigo() {
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    const nombreSeleccionado = amigos[indiceAleatorio];
+
+    if(amigos.length === 0) {
+        alert('No hay amigos para sortear en la lista!!');
+        return;
+    }
+
+    listaAmigos.style.display = 'none';
+    //botonAgregarAmigo.style.display = 'none';
+
+    resultado.textContent = `El amigo secreto es: ${nombreSeleccionado}`;
+
 }
 
 // Asignar el evento click al botón para agregar un amigo
